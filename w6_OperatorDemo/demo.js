2b970a2c28263457539c4030ebfc1399ebf633f8
Santiago Jr <fesantiagojr@email.neit.edu>
2021-11-02 17:22:43 -0400

    W6_OperatorDemo

---
+++ w6_OperatorDemo/demo.js
@@ -0,0 +1,45 @@
+//start by declaring known varibles with starting vaulues
+//varible: data stoarge device; friendly name for info
+
+var totalAmmo = 600 //ammo cache value
+var maxAmmo = 600 //hun capacity for ammo
+
+//below ensure the current gun capacity  for ammo is full before we start
+var currentAmmo = maxAmmo
+
+//shoot function -- handles updating the current ammo on screen (so as button is clicked, current ammo goes down -1)
+function shoot() {
+    if (currentAmmo > 0) {
+        //currentAmmo = currentAmmo - 1 
+        currentAmmo--
+    updatescreen()
+    }//if ends here
+}//shoot() END
+
+function updatescreen() {
+    document.getElementById("total-ammo").innerHTML ="" + totalAmmo
+    document.getElementById("current-ammo").innerHTML ="" + currentAmmo
+}//updatescreen close
+
+function getDiff(a,b) {
+    var c = a - b
+
+    return c
+}//getdiff CLOSE
+
+function reload() {
+    var difference = getDiff(maxAmmo, currentAmmo)
+    if(totalAmmo >= difference) {
+        currentAmmo += difference
+        totalAmmo -= difference
+    }
+
+    else {//when the "if" condition is FALSE, the "else" runs
+        currentAmmo += totalAmmo
+        totalAmmo -= totalAmmo //totalAmmo = 0
+    }
+
+    updatescreen()
+}//reload END
+        
+
